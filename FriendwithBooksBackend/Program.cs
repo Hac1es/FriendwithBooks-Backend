a55b22b65631d1c332342604e56d6c1a18b668e2
Nguyễn Viết Tùng <nchinhtung@gmail.com>
2025-05-19 22:28:13 +0700

    Deploy and connect to DB

--- FriendwithBooksBackend/Program.cs
+++ FriendwithBooksBackend/Program.cs
@@ -1,13 +1,26 @@
+using Npgsql;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+// Test the DB connection
+var connString = builder.Configuration.GetConnectionString("DefaultConnection");
+try
+{
+    using var conn = new NpgsqlConnection(connString);
+    conn.Open();
+    Console.WriteLine("Connect to Supabase successful.");
+}
+catch (Exception ex)
+{
+    Console.WriteLine("Failed to connect to Supabase: " + ex.Message);
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
